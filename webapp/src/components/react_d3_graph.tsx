5d9fccff1f41954d5d0630aeff450d596d0553d0
iomodo <wineson@gmail.com>
2023-05-09 17:02:16 +0400

    Add new graph lib

---
+++ webapp/src/components/react_d3_graph.tsx
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+
+import {Graph, Node} from '../types/graph';
+import {useNavigate} from 'react-router-dom';
+import {Graph as ReactD3GraphComponent} from "react-d3-graph";
+
+
+interface Props {
+    graph: Graph;
+    width: number;
+    height: number;
+}
+
+const ReactD3Graph = (props: Props) => {
+    const navigate = useNavigate();
+
+    const labelProperty = (node: Node) => {
+        return node.name;
+    }
+
+    const onClickNode = (id: string) => {
+        navigate(`/nodes/${id}`)
+    };
+
+    const config = {
+        nodeHighlightBehavior: true,
+        node: {
+            color: "lightgreen",
+            size: 120,
+            highlightStrokeColor: "blue",
+            labelProperty: labelProperty,
+        },
+        link: {
+            highlightColor: "lightblue",
+        },
+        directed: true,
+        panAndZoom: true,
+        automaticRearrangeAfterDropNode: true,
+        width: props.width,
+        height: props.height,
+    };
+
+    return (
+        <ReactD3GraphComponent
+            id="graph-id"
+            data={props.graph}
+            config={config}
+            onClickNode={onClickNode}
+        />
+    );
+}
+
+export default ReactD3Graph;
