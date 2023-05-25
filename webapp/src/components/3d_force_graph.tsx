8970b3401126360e668aae996e64da9e3086a1d0
iomodo <wineson@gmail.com>
2023-05-26 01:09:32 +0400

    Add graph RHS

--- webapp/src/components/3d_force_graph.tsx
+++ webapp/src/components/3d_force_graph.tsx
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 import ForceGraph3D from 'react-force-graph-3d';
-import ForceGraph2D from 'react-force-graph-2d';
+import ForceGraph2D, {ForceGraphMethods} from 'react-force-graph-2d';
+import { forceCollide } from 'd3';
 
-import {Graph, Node} from '../types/graph';
+import {Graph, Node, Link} from '../types/graph';
+import {GraphNodeHoverContext} from './graph';
 
 interface Props {
     graph: Graph;
-    width: number;
-    height: number;
-    d3: boolean;
+    width?: number;
+    height?: number;
+    dimension3: boolean;
 }
 
 const D3ForceGraph = (props: Props) => {
     const navigate = useNavigate();
+    const fgRef = useRef<ForceGraphMethods<Node, Link>>();
+    const {setNode} = React.useContext(GraphNodeHoverContext);
+
 
     const onNodeClick = ({id} : Node) => {
         navigate(`/nodes/${id}`)
     };
 
-    if (props.d3) {
+    useEffect(() => {
+        fgRef.current!.d3Force('collide', forceCollide(13))
+    },[]);
+
+    if (props.dimension3) {
         return (
             <ForceGraph3D
                 graphData={props.graph}
                 nodeLabel="name"
                 showNavInfo={false}
-                width={props.width}
                 height={props.height}
                 dagMode="zout"
                 linkDirectionalParticles={2}
                 linkDirectionalParticleWidth={2}
+                // d3VelocityDecay={0.3}
                 linkWidth={2}
                 onNodeClick={onNodeClick}
                 nodeAutoColorBy={"status"}
@@ -38,41 +47,52 @@ const D3ForceGraph = (props: Props) => {
     }
     return (
         <ForceGraph2D
+            ref={fgRef}
             graphData={props.graph}
             nodeLabel="description"
             width={props.width}
             height={props.height}
             linkDirectionalParticles={2}
-            linkDirectionalParticleWidth={2}
+            linkDirectionalParticleWidth={4}
             linkWidth={2}
             onNodeClick={onNodeClick}
-            dagMode='lr'
+            dagMode={"lr"}
             nodeVal={20}
             nodeCanvasObject={(node, ctx, globalScale) => {
                 const label = node.name;
-                const fontSize = 12/globalScale;
+                const fontSize = 5;
                 ctx.font = `${fontSize}px Sans-Serif`;
-                const textWidth = ctx.measureText(label).width;
-                const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
 
-                ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
-                ctx.fillRect(node.x||0 - bckgDimensions[0] / 2, node.y||0 - bckgDimensions[1] / 2, bckgDimensions[0], bckgDimensions[1]);
+                const r = 5;
+
+                ctx.beginPath();
+                ctx.arc(node.x || 0, node.y || 0, r, 0, 2 * Math.PI, false);
+                ctx.fillStyle = 'rgba(31, 120, 180, 0.92)';
+                ctx.fill();
 
                 ctx.textAlign = 'center';
                 ctx.textBaseline = 'middle';
                 ctx.fillStyle = 'black';
                 ctx.fillText(label, node.x||0, node.y||0);
-
-                node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
             }}
             nodePointerAreaPaint={(node, color, ctx) => {
+                const r = 5
                 ctx.fillStyle = color;
-                const bckgDimensions = node.__bckgDimensions;
-                if (bckgDimensions) {
-                    ctx.fillRect(node.x||0 - bckgDimensions[0] / 2, node.y||0 - bckgDimensions[1] / 2, bckgDimensions[0], bckgDimensions[1]);
-                }
+                ctx.beginPath();
+                ctx.arc(node.x || 0, node.y || 0, r, 0, 2 * Math.PI, false);
+                ctx.fill();
             }}
             nodeAutoColorBy={"status"}
+            nodeRelSize={1}
+            dagLevelDistance={50}
+            d3VelocityDecay={0.3}
+            linkDirectionalArrowLength={6}
+            linkDirectionalArrowRelPos={1}
+            onNodeHover={(node: Node | null) => {
+                if (node) {
+                    setNode(node)
+                }
+            }}
         />
     )
 
---
+++ webapp/src/components/graph_rhs.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Stack} from '@mui/material';
+import {GraphNodeHoverContext} from './graph';
+
+
+const GraphRHS = () => {
+    const {node} = React.useContext(GraphNodeHoverContext);
+
+    return (
+        <Stack>
+            <h1>Node</h1>
+            <h2>{node.name}</h2>
+            <p>{node.description}</p>
+        </Stack>
+    )
+}
+
+export default GraphRHS;
