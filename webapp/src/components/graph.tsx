d0248363c9c207a8b9dbf680d050ece142c0cc03
Shota Gvinepadze <wineson@gmail.com>
2023-05-27 20:32:16 +0400

    [GH-120] Add graph RHS (#121)
    * Add graph RHS
    
    * Fix style

--- webapp/src/components/graph.tsx
+++ webapp/src/components/graph.tsx
@@ -1,20 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import {Container, Stack} from '@mui/material';
+import React, {useEffect, useState, useRef, useLayoutEffect} from 'react';
+import {Stack} from '@mui/material';
 
 import {Client} from '../client/client';
-import {Graph} from '../types/graph';
+import {Graph, Node} from '../types/graph';
 
-import ReactD3Graph from './react_d3_graph';
+import useWindowDimensions from '../hooks/use_window_dimensions';
+
 import ForceGraph from './3d_force_graph';
+import GraphRHS from './graph_rhs';
+
+type GraphNodeHoverContextType = {
+    node: Node;
+    setNode: React.Dispatch<React.SetStateAction<Node>>;
+}
+export const GraphNodeHoverContext = React.createContext<GraphNodeHoverContextType>({node: {} as Node, setNode: ()=>{}});
 
 const GraphComponent = () => {
     const [graph, setGraph] = useState<Graph>({} as Graph);
+    const [node, setNode] = useState<Node>({} as Node);
+    const myRef = useRef<HTMLDivElement>(null);
+    const [parentWidth, setParentWidth] = useState(0);
+    const {windowHeight} = useWindowDimensions();
 
     useEffect(() => {
         Client.Graph().get().then((data) => {
             setGraph(data);
         });
     },[]);
+
+    useLayoutEffect(() => {
+        if (myRef.current){
+            setParentWidth(myRef.current.offsetWidth);
+        }
+    }, [myRef.current]);
 
 
     if (!graph.nodes){
@@ -26,27 +44,21 @@ const GraphComponent = () => {
     }
 
     return (
-        <Container>
-            <Stack width={1000} height={1800}  display={'flex'} alignItems={'center'}>
-                <ForceGraph
-                    graph={{...graph}}
-                    width={1000}
-                    height={600}
-                    d3={false}
-                />
-                <ForceGraph
-                    graph={{...graph}}
-                    width={1000}
-                    height={600}
-                    d3={true}
-                />
-                <ReactD3Graph
-                    graph={{...graph}}
-                    width={1000}
-                    height={600}
-                />
-            </Stack>
-        </Container>
+        <Stack width='100%' height={windowHeight*2/3}  display={'flex'} alignItems={'center'} direction={'row'}>
+            <GraphNodeHoverContext.Provider value={{node, setNode}}>
+                <Stack width='75%' height={windowHeight*2/3} direction={'column'} ref={myRef}>
+                    <ForceGraph
+                        graph={graph}
+                        width={parentWidth}
+                        height={windowHeight*2/3}
+                        dimension3={false}
+                    />
+                </Stack>
+                <Stack width='25%' height={windowHeight*2/3} direction={'column'}>
+                    <GraphRHS/>
+                </Stack>
+            </GraphNodeHoverContext.Provider>
+        </Stack>
     );
 }
 
