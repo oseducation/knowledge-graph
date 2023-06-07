666dd104b86ff447ff95810e8051aa5143edbffd
Rezo Shalikashvili <r.shalika08@gmail.com>
2023-06-07 17:38:35 +0200

    [GH-25]: Remove css files

--- webapp/src/pages/VerifyPage.tsx
+++ webapp/src/pages/VerifyPage.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {useLocation, useNavigate} from "react-router-dom";
+import {Box, Button, Stack, Typography} from "@mui/material";
+
+import Footer from "../components/footer";
 
 const VerifyPage: React.FC = () => {
     const location = useLocation();
@@ -10,31 +13,40 @@ const VerifyPage: React.FC = () => {
     };
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', backgroundColor: '#f0f0f0' }}>
-            <h1 style={{ color: '#444', fontSize: '2rem' }}>Almost There!</h1>
-            <p style={{ color: '#666', maxWidth: '60%', textAlign: 'center', marginBottom: '2rem' }}>
-                We just sent a verification email to:
-                <span style={{ color: '#000', fontWeight: 'bold', marginLeft: '1rem' }}>{location.state?.email}</span>.
-                Please check your inbox (and spam folder!) to confirm your account. Let&apos;s get you on board!
-            </p>
-            <button
-                onClick={navigateToLogin}
-                style={{
-                    backgroundColor: '#008CBA',
-                    color: 'white',
-                    padding: '12px 24px',
-                    borderRadius: '4px',
-                    border: 'none',
-                    cursor: 'pointer',
-                    fontSize: '1rem',
-                    transition: 'all 0.3s ease'
-                }}
-                onMouseOver={(e) => { e.currentTarget.style.backgroundColor = '#005580'; }}
-                onMouseOut={(e) => { e.currentTarget.style.backgroundColor = '#008CBA'; }}
+        <Stack>
+            <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                height="100vh"
             >
-                Back to Login
-            </button>
-        </div>
+                <Typography variant="h2">Almost There!</Typography>
+                <Typography
+                    variant="body1"
+                    maxWidth="60%"
+                    textAlign="center"
+                >
+                    We just sent a verification email to:
+                    <span style={{color: '#000', fontWeight: 'bold', marginLeft: '1rem'}}>
+                        {location.state?.email}
+                    </span>.
+                    Please check your inbox (and spam folder!) to confirm your account. Let&apos;s get you on board!
+                </Typography>
+                <Button
+                    onClick={navigateToLogin}
+                    variant="contained"
+                    color="success"
+                    sx={{
+                        m: 2,
+                        borderRadius: '4px',
+                    }}
+                >
+                    Back to Login
+                </Button>
+            </Box>
+            <Footer/>
+        </Stack>
     );
 };
 
