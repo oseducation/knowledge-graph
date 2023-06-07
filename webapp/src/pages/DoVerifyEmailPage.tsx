f36fa6304a7f5ed2ac4d48fcf92f6b43d3779eee
Rezo Shalikashvili <r.shalika08@gmail.com>
2023-06-07 15:14:58 +0200

    [GH-25]: Remove css files

--- webapp/src/pages/DoVerifyEmailPage.tsx
+++ webapp/src/pages/DoVerifyEmailPage.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import {useLocation, useNavigate} from 'react-router-dom';
+import {Box, Button, Typography} from '@mui/material';
 
 import {Client} from '../client/client';
-import './VerifyEmailPage.css';
 
 const VerifyEmailPage: React.FC = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -35,23 +35,61 @@ const VerifyEmailPage: React.FC = () => {
     }
 
     if (isLoading) {
-        return <p className="loading-message">Verifying...</p>;
+        return (
+            <Typography variant="h6" sx={{color: 'blue', textAlign: 'center', mt: 4}}>
+                Verifying...
+            </Typography>
+        );
     }
 
     if (isVerified) {
         return (
-            <div className="verification-success">
-                <p>Email verification was successful!</p>
-                <button onClick={handleButtonClick}>Go to Login</button>
-            </div>
+            <Box display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                sx={{height: '100vh'}}>
+                <Typography variant="h6" sx={{color: 'green', textAlign: 'center'}}>
+                    Email verification was successful!
+                </Typography>
+                <Button
+                    variant="contained"
+                    onClick={handleButtonClick}
+                    sx={{
+                        mt: 2,
+                        p: 1,
+                        backgroundColor: '#4CAF50',
+                        '&:hover': {backgroundColor: '#45a049'},
+                        borderRadius: 1,
+                        fontSize: '18px'
+                    }}
+                >
+                    Go to Login
+                </Button>
+            </Box>
         );
     }
 
     return (
-        <div className="verification-failed">
-            <p>Email verification failed: {errorMessage}</p>
-            <button onClick={handleButtonClick}>Go to Login</button>
-        </div>
+        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" sx={{height: '100vh'}}>
+            <Typography variant="h6" sx={{color: 'red', textAlign: 'center'}}>
+                Email verification failed: {errorMessage}
+            </Typography>
+            <Button
+                variant="contained"
+                onClick={handleButtonClick}
+                sx={{
+                    mt: 2,
+                    p: 1,
+                    backgroundColor: '#4CAF50',
+                    '&:hover': {backgroundColor: '#45a049'},
+                    borderRadius: 1,
+                    fontSize: '18px'
+                }}
+            >
+                Go to Login
+            </Button>
+        </Box>
     );
 };
 
