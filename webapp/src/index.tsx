1d0dbacfe04bc75a4dab6fe7ff1cbb6ad9bbaa4f
Shota Gvinepadze <wineson@gmail.com>
2023-07-10 19:57:40 +0400

    [GH-211] Limit app to chrome (#212)
    Limit app to chrome

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {createRoot} from 'react-dom/client';
 
-
 import {AuthProvider} from './context/auth_provider';
 import AppLayout from "./AppLayout";
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(
-    <React.StrictMode>
-        <AuthProvider>
-            <BrowserRouter>
-                <AppLayout/>
-            </BrowserRouter>
-        </AuthProvider>
-    </React.StrictMode>
-)
-
+const isChrome = window.navigator.userAgent.includes("Chrome");
+if (isChrome) {
+    root.render(
+        <React.StrictMode>
+            <AuthProvider>
+                <BrowserRouter>
+                    <AppLayout/>
+                </BrowserRouter>
+            </AuthProvider>
+        </React.StrictMode>
+    )
+} else {
+    root.render(
+        <div>For better usage, please use Chrome</div>
+    )
+}
