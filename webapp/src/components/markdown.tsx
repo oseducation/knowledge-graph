6457e7ff0a70a6133cc56884f32c24235bc88081
iomodo <wineson@gmail.com>
2023-08-13 15:44:35 +0400

    Fix style

--- webapp/src/components/markdown.tsx
+++ webapp/src/components/markdown.tsx
@@ -15,17 +15,18 @@ const Markdown = (props: Props) => {
     return (
         <ReactMarkdown
             components={{
-                code({node, inline, className, children, ...props}) {
+                code({inline, className, children, ...props}) {
                     const match = /language-(\w+)/.exec(className || '')
                     return !inline && match ?
                         (
                             <SyntaxHighlighter
                                 {...props}
-                                children={String(children).replace(/\n$/, '')}
                                 style={coy}
                                 language={match[1]}
                                 PreTag="div"
-                            />
+                            >
+                                {String(children).replace(/\n$/, '')}
+                            </SyntaxHighlighter>
                         ) : (
                             <code {...props} className={className}>
                                 {children}
