ad0c17c3195ce19debd1550edad767890bd61f33
iomodo <wineson@gmail.com>
2023-11-12 18:53:26 +0400

    Fix style

--- webapp/src/UserLayout.tsx
+++ webapp/src/UserLayout.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
 
-import {AppTheme} from "./ThemeOptions";
 import {DrawerProvider} from './context/drawer_provider';
 import {GraphProvider} from './context/graph_provider';
 import GuestLayout from './GuestLayout';
