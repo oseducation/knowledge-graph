d74e04a745863a700ecdec08fbb38da067aaf133
Shota Gvinepadze <wineson@gmail.com>
2023-11-18 14:22:08 +0400

    [GH-438] Add progress front (#441)
    Add progress front

--- webapp/src/components/overview/snippets.tsx
+++ webapp/src/components/overview/snippets.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Grid2 from '@mui/material/Unstable_Grid2';
-import {Card, CardContent, Typography, Box, useTheme} from '@mui/material';
+import {Card, Typography, Box, useTheme} from '@mui/material';
 import PunchClockOutlinedIcon from '@mui/icons-material/PunchClockOutlined';
 import LocalFireDepartmentOutlinedIcon from '@mui/icons-material/LocalFireDepartmentOutlined';
 import AutoAwesomeOutlinedIcon from '@mui/icons-material/AutoAwesomeOutlined';
@@ -23,58 +23,56 @@ const DashboardWidget = (props: Props) => {
             sx={{
                 minWidth: 275,
                 borderRadius: '16px', // rounded corners
-                // boxShadow: '0 4px 12px 0 rgba(0, 0, 0, 0.2)', // subtle shadow
+                boxShadow: '0 4px 12px 0 rgba(0, 0, 0, 0.2)', // subtle shadow
                 margin: '8px',
             }}
         >
-            <CardContent>
-                <Grid2 container spacing={2} display={'flex'} flexDirection={'row'} alignItems={'center'}>
-                    <Grid2 xs={9}>
-                        <Typography sx={{fontSize: '1.5rem', fontWeight: 'bold', margin: '8px 0'}}>
-                            {props.value}
-                        </Typography>
-                        <Typography sx={{fontSize: '1rem', color: 'text.secondary',marginBottom: '4px'}} gutterBottom>
-                            {props.title}
-                        </Typography>
+            <Box display={'flex'} flexDirection={'row'} alignItems={'center'} justifyContent={'space-between'} sx={{p:2}}>
+                <Box>
+                    <Typography sx={{fontSize: '1.5rem', fontWeight: 'bold'}}>
+                        {props.value}
+                    </Typography>
+                    <Typography sx={{fontSize: '1rem', color: 'text.secondary',mb: '4px', mt:'8px'}} gutterBottom>
+                        {props.title}
+                    </Typography>
 
-                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                            <Typography
-                                sx={{
-                                    fontSize: '0.875rem',
-                                    marginLeft: '4px',
-                                    color: props.deltaType === 'positive' ? theme.palette.success.main : theme.palette.error.main,
-                                }}
-                            >
-                                {props.deltaType === 'positive' ? '+' : '-'}{props.delta}
-                            </Typography>
-                            <Typography
-                                sx={{
-                                    fontSize: '0.875rem',
-                                    color: 'text.secondary',
-                                    marginLeft: '4px',
-                                }}>
-                                {props.secondaryText}
-                            </Typography>
-                        </Box>
-                    </Grid2>
-                    <Grid2 xs={3} display={'flex'} alignContent={'center'} m={0} p={0}>
-                        <Box
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Typography
                             sx={{
-                                background: props.iconBackground,
-                                width: {xs: '40px', md: '48px', lg: '64px'},
-                                height: {xs: '40px', md: '48px', lg: '64px'},
+                                fontSize: '0.875rem',
+                                marginLeft: '4px',
+                                color: props.deltaType === 'positive' ? theme.palette.success.main : theme.palette.error.main,
                             }}
-                            display={'flex'}
-                            alignContent={'center'}
-                            alignItems={'center'}
-                            justifyContent={'center'}
-                            borderRadius={'16px'}
                         >
-                            {props.icon}
-                        </Box>
-                    </Grid2>
-                </Grid2>
-            </CardContent>
+                            {props.deltaType === 'positive' ? '+' : '-'}{props.delta}
+                        </Typography>
+                        <Typography
+                            sx={{
+                                fontSize: '0.875rem',
+                                color: 'text.secondary',
+                                marginLeft: '4px',
+                            }}>
+                            {props.secondaryText}
+                        </Typography>
+                    </Box>
+                </Box>
+                <Box display={'flex'} alignContent={'center'} m={0} p={0}>
+                    <Box
+                        sx={{
+                            background: props.iconBackground,
+                            width: {xs: '40px', md: '48px', lg: '64px'},
+                            height: {xs: '40px', md: '48px', lg: '64px'},
+                        }}
+                        display={'flex'}
+                        alignContent={'center'}
+                        alignItems={'center'}
+                        justifyContent={'center'}
+                        borderRadius={'16px'}
+                    >
+                        {props.icon}
+                    </Box>
+                </Box>
+            </Box>
         </Card>
     );
 }
