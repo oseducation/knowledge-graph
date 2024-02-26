a65caa845806895599d9fdbd5f037ce0a6c1512a
iomodo <wineson@gmail.com>
2024-02-26 19:28:56 +0400

    Add log for testing

--- webapp/src/components/bot/create_post.ts
+++ webapp/src/components/bot/create_post.ts
@@ -70,6 +70,7 @@ export const constructBotPost = (posts: Post[], node: NodeWithResources | null, 
 }
 
 export const getBotPostActions = (posts: Post[] | null, node: NodeWithResources) => {
+    console.log('getBotPostActions', posts, node);
     if (!posts || !node) {
         return [];
     }
