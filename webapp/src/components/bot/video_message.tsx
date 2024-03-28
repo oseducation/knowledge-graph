484921ecb684374eef322bb2894e7a8628c96945
iomodo <wineson@gmail.com>
2024-03-28 13:56:57 +0400

    Add persistant video

--- webapp/src/components/bot/video_message.tsx
+++ webapp/src/components/bot/video_message.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import {Box} from '@mui/material';
+import React, {useEffect, useRef, useState} from 'react';
+import {Box, IconButton} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 import useAuth from '../../hooks/useAuth';
 import {Client} from '../../client/client';
@@ -13,6 +14,9 @@ interface Props {
     post: Post;
     isLast: boolean;
     scrollToBottom: () => void;
+    addScrollListener?: (a: ()=>void) => void;
+    removeScrollListener?: (a: ()=>void) => void;
+    isLastVideo?: boolean;
 }
 
 const VideoMessage = (props: Props) => {
@@ -21,8 +25,31 @@ const VideoMessage = (props: Props) => {
     const videoKey = props.post.props.video_key;
     const start = props.post.props.start;
     const length = props.post.props.length;
+    const videoRef = useRef<HTMLDivElement>(null);
+    const [isFloating, setIsFloating] = useState<boolean>(false);
+    const [open, setOpen] = useState(false);
 
     const {message} = useMessageReveal(props.post.message, props.isLast, props.scrollToBottom);
+
+    useEffect(() => {
+        if (!props.isLastVideo || !props.addScrollListener || !props.removeScrollListener){
+            return;
+        }
+        props.addScrollListener(handleScroll);
+        return () => {
+            if (props.removeScrollListener) {
+                props.removeScrollListener(handleScroll);
+            }
+        }
+    }, []);
+
+    const handleScroll = () => {
+        if (!videoRef.current) return;
+
+        const {bottom} = videoRef.current.getBoundingClientRect();
+        setIsFloating(bottom < 50);
+    };
+
 
     if (props.post.post_type !== PostTypeVideo) {
         return null;
@@ -41,25 +68,54 @@ const VideoMessage = (props: Props) => {
             Client.Video().videoFinished(videoKey);
         }
     }
+    const floatingStyle = isFloating && !open && !props.isLast? {
+        position: 'fixed',
+        top: '40px',
+        right: '0',
+        width: '300px',
+        height: 'auto',
+        zIndex: 1000,
+        margin: '20px'
+    } : {};
 
     return (
         <Box display={'flex'} flexDirection={'column'}>
             <Markdown text={message}/>
-            <Box sx={{
-                width: {xs: '280px', sm: '500px', md: '600px', lg: '800px'},
-                height: {xs: '200px', sm: '400px', md: '500px', lg: '600px'}
-            }}>
-                <VideoPlayer
-                    videoKey={videoKey}
-                    start={start}
-                    length={length}
-                    width={'100%'}
-                    height={'100%'}
-                    autoplay={false}
-                    onVideoStarted={onVideoStarted}
-                    onVideoEnded={onVideoEnded}
-                />
-            </Box>
+            <div ref={videoRef}>
+                {isFloating &&
+                    <Box sx={{
+                        width: {xs: '280px', sm: '500px', md: '600px', lg: '800px'},
+                        height: {xs: '200px', sm: '400px', md: '500px', lg: '600px'},
+                    }}/>
+                }
+                <Box sx={{
+                    width: {xs: '280px', sm: '500px', md: '600px', lg: '800px'},
+                    height: {xs: '200px', sm: '400px', md: '500px', lg: '600px'},
+                    ...floatingStyle
+                }}>
+                    <VideoPlayer
+                        videoKey={videoKey}
+                        start={start}
+                        length={length}
+                        width={'100%'}
+                        height={'100%'}
+                        autoplay={false}
+                        onVideoStarted={onVideoStarted}
+                        onVideoEnded={onVideoEnded}
+                    />
+                    {isFloating &&
+                        <IconButton
+                            onClick={() => {
+                                setOpen(true);
+                            }}
+                            size="small"
+                            sx={{position: 'absolute', top: '-20px', right: '-20px', margin: '-2px'}}
+                        >
+                            <CloseIcon fontSize="small" />
+                        </IconButton>
+                    }
+                </Box>
+            </div>
         </Box>
     );
 }
